9fae0feeefb082b46fe9252347fc902ba1bfd6a5
hoang <vinhyenvodoi98@gmail.com>
2019-05-22 15:47:53 +0700

    update interface storeNumber and showNumber

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Web3 = require('web3')
+const express = require('express')
+var bodyParser = require("body-parser");
 const SimpleStore = require('./build/contracts/SimpleStore.json')
 const {
   LoomProvider, Client,
   Contract, Address, LocalAddress, CryptoUtils
 } = require ('loom-js')
+
+const app = express();
+app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'jade');
 
 const privateKey = CryptoUtils.generatePrivateKey()
 const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey)
@@ -14,32 +22,51 @@ const client = new Client(
   'ws://127.0.0.1:46658/websocket',
   'ws://127.0.0.1:46658/queryws',
 )
-const main = async() =>  {
-  // The address for the caller of the function
-  const from = LocalAddress.fromPublicKey(publicKey).toString()
-  console.log("from = " + from);
 
-  // Instantiate web3 client using LoomProvider
-  const web3 = new Web3(new LoomProvider(client, privateKey))
+// The address for the caller of the function
+const from = LocalAddress.fromPublicKey(publicKey).toString()
+console.log("from = " + from);
 
-  // const contractAddress = '0x62C436B6f3f028cF1eb14BDBBc0eaF0c63f62B0E'
-  const contractAddress = SimpleStore.networks["13654820909954"].address;
+// Instantiate web3 client using LoomProvider
+const web3 = new Web3(new LoomProvider(client, privateKey))
 
-  console.log(contractAddress);
-  // Instantiate the contract and let it ready to be used
-  const contract = new web3.eth.Contract(SimpleStore.abi, contractAddress, {from})
-  // await contract.set(47, {from});
-  // Set the value 47
-  await contract.methods.set(30).send({from })
-  // Get the value 47
-  await contract.methods.get().call({from : from},(error,result)=>{
-    console.log("result = " + result);
-  })
+// const contractAddress = '0x62C436B6f3f028cF1eb14BDBBc0eaF0c63f62B0E'
+const contractAddress = SimpleStore.networks["13654820909954"].address;
 
+console.log(contractAddress);
+// Instantiate the contract and let it ready to be used
+const contract = new web3.eth.Contract(SimpleStore.abi, contractAddress, {from})
+
+set = (storeNumber) => {
+  contract.methods.set(storeNumber).send({from })
+}
+
+get = () => {
+  contract.methods.get().call({from : from})
+  .then(result=>(result))
+}
+
+event = () => {
   contract.events.NewValueSet({}, (error, event) => {
     console.log('New value set = ', event)
   })
   .on('error',console.error)// The address for the caller of the function
 }
 
-main();
+app.get('/',(req,res)=>{
+  contract.methods.get().call({from : from})
+  .then(result=>{
+    console.log(result);
+    res.render('index',{ number : result});
+  })
+})
+
+app.post('/',(req,res)=>{
+  // console.log(req.body);
+  set(parseInt(req.body.storeNumber));
+  res.redirect('/');
+})
+
+app.listen(3000);
+
+console.log("app is running");
